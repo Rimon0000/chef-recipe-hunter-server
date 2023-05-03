c5ba7ed5d1d76753517194ee75eecb33cfd53d32
Your Name <Rimon0000>
2023-05-03 13:15:14 +0600

    load single data by id

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 5000
+
+const chef = require('./data/chef.json')
+
+app.use(cors())
+
+app.get('/chef', (req, res) =>{
+  res.send(chef)
+})
+
+//id base data
+app.get('/chef/:id', (req, res) =>{
+  const id = req.params.id 
+  // console.log(id)
+  const selectedChef = chef.find(c => c.id == id)
+  res.send(selectedChef)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
   console.log(`chef running on port ${port}`)
